953944c4fa2eb391bf90b3a1ace5b786ccc1e2a2
Akbota Kaliollayeva <kaliollaeva.b@gmail.com>
2025-02-18 22:48:06 +0600

    first 2

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,29 @@
 // @todo: Темплейт карточки
+  const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
+const cardList = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
+function createCard(card, deleteCard) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
 
+  cardElement.querySelector(".card__title").textContent = card.name;
+  cardElement.querySelector(".card__image").src = card.link;
+
+  const buttonDelete = cardElement.querySelector(".card__delete-button");
+  buttonDelete.addEventListener("click", function () {
+    deleteCard(cardElement);
+  });
+
+  return cardElement;
+}
 // @todo: Функция удаления карточки
-
+function deleteCard(card) {
+  card.remove();
+}
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function (card) {
+  const resultCard = createCard(card, deleteCard);
+  cardList.append(resultCard);
+});
